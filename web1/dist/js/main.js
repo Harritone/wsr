4b68a02779b9018b79f90bb9bd7e3d2e2cab1563
Harritone <yourrasik@gmail.com>
2020-11-02 20:22:21 +1000

    Added responsiveness

--- web1/dist/js/main.js
+++ web1/dist/js/main.js
@@ -1,14 +1,22 @@
 class Dropdown {
-  constructor(container) {
+  constructor(container, trigger, content) {
     this.container = container;
-    this.trigger = container.querySelector(".dropdown__trigger");
-    this.content = container.querySelector(".dropdown__content");
+    this.trigger = trigger || container.querySelector(".dropdown__trigger");
+    this.content = content || container.querySelector(".dropdown__content");
+    this.caret = container.querySelector(".dropdown__caret");
   }
 
   init() {
     this.trigger.addEventListener("click", () => {
-      this.trigger.classList.toggle("active");
-      this.content.classList.toggle("active");
+      if (this.trigger) {
+        this.trigger.classList.toggle("active");
+      }
+      if (this.content) {
+        this.content.classList.toggle("active");
+      }
+      if (this.caret) {
+        this.caret.classList.toggle("active");
+      }
     });
   }
 }
@@ -18,3 +26,12 @@ dropdowns.forEach((dropdown) => {
   const instance = new Dropdown(dropdown);
   instance.init();
 });
+
+const trigger = document.querySelector(".resp__trigger");
+const content = document.querySelector(".resp__navigation");
+// trigger.addEventListener("click", () => {
+//   content.classList.toggle("active");
+// });
+const container = document.querySelector(".resp");
+const navdrop = new Dropdown(container, trigger, content);
+navdrop.init();
